96130380d5e82eb0ee2fcc69c1e9d0cadfe89706
Karabo H <carchelivia@gmail.com>
2025-07-08 20:39:17 +0200

    Add 3-value_argument.js update

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,13 +1,8 @@
-
 function say(a, b) {
-	if (a === undefined) {console.log("No argument");}
-	else if (b === undefined) {console.log(a);}
-	else {
-	console.log(a + " " + b);}
-}
-let a = "HBTN";
-let n = "cool";
-say(a)
-say(a,n)
+	if (a === undefined) {console.log("No argument");
+	} else if (b === undefined) { console.log("HBTN");
+	} else {console.log("HBTN cool");
+	}}
 say()
-
+say("anything")
+say("anything", "else")
