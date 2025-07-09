eb5db1a3a89e58854b17bd0ae705557f66c82a7a
Karabo H <carchelivia@gmail.com>
2025-07-09 16:32:04 +0200

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 function integer() {
         const checker = Number(args[0], 10);
         if (Number.isInteger(checker)) {
-        console.log("My Number:" + " " + checker);
+        console.log("My number:" + " " + checker);
 } else {
         console.log("Not a number");
 }}
