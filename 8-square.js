ab2c75116d12f3be03478aeb65f6854d8ceb4219
Karabo H <carchelivia@gmail.com>
2025-07-09 16:08:19 +0200

    Add 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (isNaN(size)) {
+	console.log("Missing size");
+} else {
+	for (let i = 0; i < size; i++) {
+		console.log("x".repeat(size));
+	}
+}
+
