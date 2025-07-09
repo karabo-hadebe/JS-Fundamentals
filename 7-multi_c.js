efef2a42a86235159a5bf89db88dfb2a84e77de8
Karabo H <carchelivia@gmail.com>
2025-07-09 16:54:31 +0200

    just added everything

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2)
 const count = parseInt(args[0]);
 
-if (isNaN(count) || count < 0) {
+if (isNaN(count)) {
 	console.log("Missing number of occurences");
-} else {
+} else if (count >= 0) {
 	let lines = [];
 	for (let i = 0; i < count; i++) { 
 		lines.push("C is fun");
