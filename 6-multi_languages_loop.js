54f02b8e8ef9a1c46eb30c906cc27d4b47ca5172
Karabo H <carchelivia@gmail.com>
2025-07-09 16:22:55 +0200

    just added everything

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let result = [];
+for (let i = 0; i < lines.length; i++) {
+	result.push(lines[i]);}
+
+console.log(result.join("\n"));
