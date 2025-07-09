e4f00fdadd87abe53195b94821bf5733920662a5
Karabo H <carchelivia@gmail.com>
2025-07-09 16:46:20 +0200

    Add 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2)
+const count = parseInt(args[0]);
+
+if (isNaN(count)) {
+	console.log("Missing number of occurences");
+} else {
+	let lines = [];
+	for (let i = 0; i < count; i++) { 
+		lines.push("C is fun");
+	}
+	console.log(lines.join("\n"));
+}
