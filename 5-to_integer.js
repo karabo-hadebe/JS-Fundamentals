62c1ea24d522471c8934450ffe278918519e2729
Karabo H <carchelivia@gmail.com>
2025-07-09 16:25:48 +0200

    just added everything

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,8 @@
 const args = process.argv.slice(2);
 
 function integer() {
-        const checker = parseInt(args[0], 10);
-        if (!isNaN(checker)) {
+        const checker = Number(args[0], 10);
+        if (Number.isInteger(checker)) {
         console.log("My Number:" + " " + checker);
 } else {
         console.log("Not a number");
